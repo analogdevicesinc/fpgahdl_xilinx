dd75dd407b0191d028421473c3ee4b2f0aaec526
Lars-Peter Clausen <lars@metafoo.de>
2014-03-07 18:43:34 +0100

    Remove outdated axi_ad9122 and axi_ad9643 versions
    All projects now use v6 of these cores, v1 can be removed.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- cf_lib/edk/pcores/axi_ad9643_v1_00_a/data/axi_ad9643_v2_1_0.tcl
+++
@@ -1,8 +0,0 @@
-
-proc run_coregen {mhsinst} {  
-  set core_dir [file join [xget_hw_pcore_dir $mhsinst] ..]
-  set adi_common_dir [file join $core_dir .. adi_common_v1_00_a]
-  set adi_common_dir [file normalize $adi_common_dir]
-  source [file join $adi_common_dir data generate_ngc_and_v.tcl]
-  generate_ngc_and_v $adi_common_dir "ad_dcfilter_1"
-}  
